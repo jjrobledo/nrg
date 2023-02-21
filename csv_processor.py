25dc02e121bd5a4834c93958b119cc3f9cf989cb
Jesse Robledo <robledo.josej@gmail.com>
2023-02-21 14:20:30 -0700

    refactored get_data_files to return an an array containing file info

--- csv_processor.py
+++ csv_processor.py
@@ -26,12 +26,14 @@ import pandas as pd
 import os
 import re
 
+df = pd.DataFrame()
+
 
 def get_data_files():
     path = os.getcwd() + '/shape/csv'
     files = os.listdir(path)
 
-    matching_prefix_files = {}
+    matching_prefix_files = []
 
     # loop through files in the directory
     for file in files:
@@ -39,27 +41,50 @@ def get_data_files():
         prefix_match = re.match(r'^(\d+)_', file)
         if prefix_match:
             prefix = prefix_match.group(1)
-            # add file to matching prefix files dictionary
-            if prefix in matching_prefix_files:
-                matching_prefix_files[prefix].append(file)
-            else:
-                matching_prefix_files[prefix] = [file]
+            extension = os.path.splitext(file)[1]
+            # add file to matching prefix files list
+            found = False
+            for prefix_files in matching_prefix_files:
+                if prefix_files['prefix'] == prefix:
+                    prefix_files['files'][extension] = file
+                    found = True
+                    break
+            if not found:
+                matching_prefix_files.append({'prefix': prefix, 'files': {extension: file}})
 
     return matching_prefix_files
+
+
+def csv_cleaner(df):
+    df = df.drop(
+        ['sPerfOrgIn', 'sLeadAgenc', 'sLeadAge_1', 'sSponsor', 'iAllResour', 'bGISAccept', 'iStatusCur', 'sDescrip',
+         'geometry'], axis=1)
+
+    return df
+
+
+def xl_cleaner(df):
+    df = df.rename(columns={'NMCRIS Activity #': 'nmcris_number', 'Author': 'author'})
+    df = df.drop(['performing_organization', 'total_acres', 'report_title'], axis=1)
+
+    return df
 
 
 def nmcris_file_processor():
     # get a list of all filenames and get the unique parcel ids
     file_dictionary = get_data_files()
 
-    for parcel_id, files in file_dictionary.items():
-        print(parcel_id, files)
+    for dictionary in file_dictionary:
+        print(dictionary)
+
+    print(dictionary)
 
     # try opening a csv and xlsx for each parcel_id and assign each to a variable
+
     # clean the csv
     # clean the xlsx
     # join the csv to the xlsx
-    # merge the cleaned and joined df to the emshapefiles = []
+    # merge the cleaned and joined df to the new df
     #
 
 
