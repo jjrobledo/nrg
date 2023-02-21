d631ba11050ba7ed794738db2413561220e2cba8
Jesse Robledo <robledo.josej@gmail.com>
2023-02-20 18:01:53 -0700

    will try to create csv directory and output csv files

--- shape_to_nmcris_number_csv.py
+++ shape_to_nmcris_number_csv.py
@@ -1,19 +1,32 @@
-import sys
+import os
 import geopandas as gpd
 
 
-def shapefile_to_csv(filename):
+def shapefile_to_csv(filename, path):
+    os.chdir(path)
+    directory = 'csv'
+    output_directory = os.path.join(path, directory)
+
     df = gpd.read_file(filename)
     output_filename = filename.split('.')[0] + '.csv'
-    with open(output_filename, 'w') as f:
-        for nmcris_number in df.iActivityN.tolist():
 
+    try:
+        os.mkdir(output_directory)
+        print("Output folder created")
+    except Exception:
+        pass
+
+    print(output_directory, output_filename)
+    with open(os.path.join(output_directory + '/' + output_filename), 'w') as f:
+        for nmcris_number in df.iActivityN.tolist():
             f.write(str(nmcris_number) + ', ')
 
 
-for filename in sys.argv[1:]:
-    if filename.split('.')[-1] != 'shp':
-        pass
-    else:
-        print(f"Converting {filename} to csv")
-        shapefile_to_csv(filename)
+def process_shapefiles(filename_list, path):
+    for filename in filename_list:
+        print(filename)
+        if filename.split('.')[-1] != 'shp':
+            pass
+        else:
+            print(f"Converting {filename} to csv")
+            shapefile_to_csv(filename, path)
